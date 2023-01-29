ab2e9161605347b60e381ec5e3da47de8e9e7182
electrolyte-orb <electrolyte.spdf@outlook.com>
2023-01-29 12:28:28 +0000

    added Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 "use client";
 import { CssBaseline, GeistProvider, useTheme } from "@geist-ui/core";
+import { Inter } from "@next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  // default, can also use "swap" to ensure custom font always shows
+  display: "swap",
+});
+
 import Navbar from "./Navbar";
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
 }) {
   const theme = useTheme();
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
