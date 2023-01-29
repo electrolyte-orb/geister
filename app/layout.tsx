2595faec1cbdfb0b57392380ae1860a9883c212e
electrolyte-orb <electrolyte.spdf@outlook.com>
2023-01-29 12:14:39 +0000

    added homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import './globals.css'
-
+"use client";
+import { CssBaseline, GeistProvider, useTheme } from "@geist-ui/core";
+import Navbar from "./Navbar";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const theme = useTheme();
   return (
     <html lang="en">
       {/*
@@ -12,7 +14,24 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <GeistProvider>
+          <CssBaseline />
+          <Navbar />
+          <main
+            style={{
+              maxWidth: theme.layout.pageWidth,
+              width: "100%",
+              display: "flex",
+              padding: `0 ${theme.layout.gap}`,
+              margin: `0 auto`,
+              boxSizing: "border-box",
+            }}
+          >
+            {children}
+          </main>
+        </GeistProvider>
+      </body>
     </html>
-  )
+  );
 }
