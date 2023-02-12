8c2aa16d791a8ef38c6055bb8ad75be3d0a86b98
electrolyte-orb <electrolyte.spdf@outlook.com>
2023-02-12 16:28:39 +0000

    added volumes

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,8 @@ WORKDIR /app
 COPY ./docker-compose.yml .
 COPY ./run.sh .
 
+EXPOSE 3000
+
+VOLUME ["/var/run/docker.sock", "/var/run/docker.sock"]
+
 CMD [ "/bin/sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 docker pull mongo
-docker run -d mongo
+docker pull ghcr.io/electrolyte-orb/back4app-image-geister:latest
+docker run -d -p 3000:3000 ghcr.io/electrolyte-orb/back4app-image-geister:latest
